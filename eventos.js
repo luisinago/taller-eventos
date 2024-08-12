10bfa9f1ddbfbe4e22321ea5d624d79d92f03ac2
luisinago <luisinago@gmail.com>
2024-08-12 20:52:10 -0300

    Agregué javascript

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let div = document.getElementById("elDiv");
+    div.addEventListener("click", function(){
+        alert("Hola! Soy el div");
+    })
+})
